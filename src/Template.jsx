1846b2a10a08c905ff84cfd12a0734a24a1e16ea
Jl Sut Pi <sutpi070@gmail.com>
2025-12-16 00:11:53 +0700

    finish footer responsive

--- src/Template.jsx
+++ src/Template.jsx
@@ -6,9 +6,9 @@ import Header from "./components/Header";
 const Template = () => {
   return (
     <div>
-      <Header />
-      <Outlet />
-      {/* <Footer /> */}
+      {/* <Header /> */}
+      {/* <Outlet /> */}
+      <Footer />
     </div>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,57 +5,94 @@ import { PiInstagramLogoFill } from "react-icons/pi";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  //py-[15px]  lg:py-[30px]
   return (
-    <div className="px-[75px] mt-auto pt-[70px] pb-10 gap-[60px] flex flex-col">
+    <footer className=" xl:px-[75px] lg:px-[45px]   md:px-[30px] px-5  md:pt-10 pt-[25px] lg:pt-[70px] mt-auto lg:pb-10 md:pb-[30px] pb-5 lg:gap-[60px] gap-5 md:gap-10 flex flex-col">
       {/* Top info */}
-      <div className=" flex justify-between">
-        <h1 className=" max-w-111 font-medium text-[40px] leading-12 tracking-[0]">
+      <div className=" flex-col gap-5 lg:gap-0 lg:flex-row  justify-center items-center lg:items-stretch  flex lg:justify-between">
+        <h1 className="w-full  lg:max-w-111 font-medium md:text-[30px]  text-[24px]  lg:text-[40px] leading-10 lg:leading-12 tracking-[0]">
           Our platform is trusted by millions & features best updated movies all
           around the world.
         </h1>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col-reverse md:flex-col gap-5 lg:gap-0   ">
           <ul className="flex gap-2">
-            <Link to={"/"}>
-              <li className=" font-normal text-[20px] leading-8">Home /</li>
-            </Link>{" "}
-            <Link>
-              <li className=" font-normal text-[20px] leading-8">Discover /</li>
-            </Link>
-            <Link>
-              <li className=" font-normal text-[20px] leading-8">
-                Influencer /
-              </li>
-            </Link>
-            <Link>
-              <li className=" font-normal text-[20px] leading-8">Release</li>
-            </Link>
+            <li>
+              <Link
+                className="hover:opacity-70 font-normal text-[20px] leading-8"
+                to={"/"}
+              >
+                Home /
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:opacity-70 font-normal text-[20px] leading-8"
+                to={"/discover"}
+              >
+                Discover /
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:opacity-70 font-normal text-[20px] leading-8"
+                to={"/about"}
+              >
+                About /
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:opacity-70 font-normal text-[20px] leading-8"
+                to={"/movie"}
+              >
+                Release /
+              </Link>
+            </li>
           </ul>
-          <div className="flex self-end mt-auto gap-10">
-            <PiInstagramLogoFill size={32} className=" cursor-pointer" />
-            <FaFacebookSquare size={32} className=" cursor-pointer" />
-            <FaTwitterSquare size={32} className=" cursor-pointer" />
-            <AiFillGoogleSquare size={32} className=" cursor-pointer" />
+          <div className="flex self-start lg:self-end mt-auto gap-10">
+            <PiInstagramLogoFill
+              size={32}
+              className=" cursor-pointer hover:opacity-70"
+              aria-label="Instagram"
+            />
+            <FaFacebookSquare
+              size={32}
+              className=" cursor-pointer hover:opacity-70"
+              aria-label="Facebook"
+            />
+            <FaTwitterSquare
+              size={32}
+              className=" cursor-pointer hover:opacity-70"
+              aria-label="X or Twitter"
+            />
+            <AiFillGoogleSquare
+              size={32}
+              className=" cursor-pointer hover:opacity-70"
+              aria-label="Google"
+            />
           </div>
         </div>
       </div>
 
       {/* Info */}
-      <div className=" flex items-center justify-between">
+      <div className=" flex-col mt-2 lg:mt-0 gap-5 lg:gap-0 lg:flex-row flex items-center justify-between">
         <ul className="flex gap-[26px]">
-          <li className=" font-normal text-[14px] leading-[22px] ">
+          <li className=" cursor-pointer hover:opacity-70 font-normal text-[14px] leading-[22px] ">
             Privacy policy
           </li>
-          <li className=" font-normal text-[14px] leading-[22px] ">
+          <li className=" cursor-pointer hover:opacity-70 font-normal text-[14px] leading-[22px] ">
             Term of service
           </li>
-          <li className=" font-normal text-[14px] leading-[22px] ">Language</li>
+          <li className=" cursor-pointer hover:opacity-70 font-normal text-[14px] leading-[22px] ">
+            Language
+          </li>
         </ul>
         <div className=" font-normal text-[14px] leading-[22px]">
-          {new Date().getFullYear()} &copy;
+          &copy; {new Date().getFullYear()}
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
